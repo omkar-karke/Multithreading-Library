42ada97b376bd70605a51ccd3441c2a9b7a10d77
Omkar Karke <omkarkarke25@gmail.com>
2022-05-19 23:45:59 +0530

    added thread function declarations

--- one-one/thread.h
+++ one-one/thread.h
@@ -3,6 +3,8 @@
 #include<setjmp.h>
 #define CLONE_FLAGS CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SYSVSEM | CLONE_SIGHAND | CLONE_THREAD | CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID
 
+enum join_status {DETACHED, JOINABLE, JOINED};
+
 typedef struct thread_control_block{
     int tid;
     void *stack_beginning;
@@ -15,3 +17,15 @@ typedef struct thread_control_block{
     int status_of_join;
     int joined_on;
 }thread_control_block;
+
+
+typedef unsigned long thread;
+
+void clean_thread_resources(thread_control_block *tcb);
+
+int start_routine(void *);
+int thread_create(thread *, void *(*f)(void *), void *, int join_status);
+int thread_join(thread, void **);
+int send_signal_all(int signum);
+int thread_kill(thread tcb, int signum);
+void thread_exit(void *retval);
