90224662711426f3c13a82e189c34367e9c9f5d2
Omkar Karke <omkarkarke25@gmail.com>
2022-05-19 23:50:30 +0530

    Implemented mutex-lock

--- one-one/lock.c
+++ one-one/lock.c
@@ -2,8 +2,7 @@
 #include <unistd.h>
 #include "lock.h"
 
-static int test_and_set(int *value)
-{
+static int test_and_set(int *value){
     int r, set = 1;
     asm("lock xchgl %0, %1"
         : "+m"(*value), "=a"(r)
@@ -12,18 +11,36 @@ static int test_and_set(int *value)
     return r;
 }
 
-void spinlock_init(spinlock *sl)
-{
+void spinlock_init(spinlock *sl){
     sl->flag = 0;
 }
 
-void thread_lock(spinlock *sl)
-{
+void thread_lock(spinlock *sl){
     while (test_and_set(&sl->flag))
         ;
 }
 
-void thread_unlock(spinlock *sl)
-{
+void thread_unlock(spinlock *sl){
     sl->flag = 0;
+}
+
+void mutexlock_init(mutexlock *lock){
+    lock->flag = 0;
+}
+
+void thread_mutex_lock(mutexlock *cm){
+    thread_lock(&(cm->sl));
+    while (cm->flag){
+        thread_unlock(&(cm->sl));
+        sleep(1);
+        thread_lock(&(cm->sl));
+    }
+    cm->flag = 1;
+    thread_unlock(&(cm->sl));
+}
+
+void thread_mutex_unlock(mutexlock *cm){
+    thread_lock(&(cm->sl));
+    cm->flag = 0;
+    thread_unlock(&(cm->sl));
 }
--- one-one/lock.h
+++ one-one/lock.h
@@ -1,8 +1,20 @@
-typedef struct spinlock
-{
+typedef struct spinlock{
     int flag;
 } spinlock;
+
+typedef struct mutexlock{
+    spinlock sl;
+    int flag;
+} mutexlock;
+
+mutexlock LL_lock, initializer_lock;
+
+static int test_and_set(int *value);
 
 void spinlock_init(spinlock *sl);
 void thread_lock(spinlock *sl);
 void thread_unlock(spinlock *sl);
+
+void mutexlock_init(mutexlock *lock);
+void thread_mutex_lock(mutexlock *cm);
+void thread_mutex_unlock(mutexlock *cm);
